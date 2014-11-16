f013a7f233d150208ab89c2479352da525cad86f
Marcello Traversi <me@marcellotraversi.com>
2014-11-16 12:00:22 +0100

    Color ghost.

---
+++ codewars.com/8kyu/color_ghost.py
@@ -0,0 +1,25 @@
+"""
+color_ghost.py
+
+Language: python
+Version: 2.7.5
+OS: OS X
+Author: Marcello Traversi
+E-mail: me@marcellotraversi.com
+Kyu: 8
+Problem:
+    Create a class Ghost
+    Ghost objects are instantiated without any arguments.
+    Ghost objects are given a random color attribute of
+    "white" or "yellow" or "purple" or "red" when instantiated
+Example:
+    ghost = Ghost()
+    ghost.color  #=> "white" or "yellow" or "purple" or "red"
+"""
+import random
+
+
+class Ghost(object):
+    def __init__(self):
+        seq = ('white', 'yellow', 'purple', 'red')
+        self.color = random.choice(seq)
