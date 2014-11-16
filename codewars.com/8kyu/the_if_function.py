b71b4b9f2804cecbcfce08229205d1f8df7f130f
Marcello Traversi <me@marcellotraversi.com>
2014-11-16 11:15:31 +0100

    The 'if' function.

---
+++ codewars.com/8kyu/the_if_function.py
@@ -0,0 +1,31 @@
+"""
+multiply.py
+
+Language: python
+Version: 2.7.5
+OS: OS X
+Author: Marcello Traversi
+E-mail: me@marcellotraversi.com
+Kyu: 8
+Problem:
+    Who likes keywords? Nobody likes keywords, so why use them?
+    You know what keyword I use too much? if! We should make
+    a function called _if, with its arguments as a logical test
+    and two functions/lambdas where the first function is executed
+    if the boolean is true, and the second if it's false,
+    like an if/else statement, so that we don't have to mess around
+    with those nasty keywords! Even so, It should support
+    truthy/falsy types just like the keyword.
+Example:
+    def truthy(): print("True")
+    def falsey(): print("False")
+    _if(True, truthy, falsey)
+    # Prints 'True' to the console.
+"""
+
+
+def _if(bool, truthy, falsey):
+    if bool:
+        return truthy()
+    else:
+        return falsey()
