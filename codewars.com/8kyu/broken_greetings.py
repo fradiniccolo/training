20d61b4b20dc1bd21c7e19c61640b0c9b2955e23
Marcello Traversi <me@marcellotraversi.com>
2014-11-16 11:16:30 +0100

    Broken greetings.

---
+++ codewars.com/8kyu/broken_greetings.py
@@ -0,0 +1,21 @@
+"""
+broken_greetings.py
+
+Language: python
+Version: 2.7.5
+OS: OS X
+Author: Marcello Traversi
+E-mail: me@marcellotraversi.com
+Kyu: 8
+Problem:
+    Correct this code, so that the greet function returns the expected value.
+Example:
+"""
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+  
+    def greet(self, other_name):
+        return "Hi {0}, my name is {1}".format(other_name, self.name)
